df4c895a87401702e88e3c6496960a388bb0a5a5
Alex Kostin <askostin.mipt@yandex.ru>
2022-09-20 16:19:14 +0300

    doc(dates_conversion): add description to both py and ipynb files

--- dates_conversion.py
+++ dates_conversion.py
@@ -1,3 +1,8 @@
+"""
+Convert text file with dates in format DD.MM.YYYY are writen in lines
+and separated by ',', and where each line relates to the selected month of the given year, to the format:
+'@Month_name(RU)\n' + listof_dates(DD.MM.YY)
+"""
 import re
 
 months = {
@@ -12,5 +17,3 @@ with open('./dates.txt', 'r', encoding='utf8') as file_in, open('./dates2.txt', 
         first_date = re.search(r'\d{0,1}\d\.(\d\d)\.(\d\d\d\d)', monthly_note)
         file_out.write(f"{months[int(first_date.group(1))]} {first_date.group(2)}:\n")
         file_out.write(re.sub(r'20(\d\d)', r'\1', monthly_note))
-
-
